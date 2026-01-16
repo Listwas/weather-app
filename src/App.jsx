4444d8f76fd664e090af67e3559ca38b50a9670c
listwas <listwa.patrykkr@gmail.com>
2026-01-16 19:23:51 +0100

    refactor: app polishing etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { mockCities } from "./data/mockWeatherData";
-import Header from "./components/Header";
-import CityGrid from "./components/CityGrid";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Favorites.jsx";
 import "./styles/App.css";
 
-function App() {
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const saved = localStorage.getItem("searchQuery");
+    if (saved) setSearchQuery(saved);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("searchQuery", searchQuery);
+  }, [searchQuery]);
+
   return (
-    <div className="center">
-      <div>
-        <Header />
-        <CityGrid cities={mockCities} />
-      </div>
-    </div>
+    <Provider store={store}>
+      <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <Routes>
+        <Route path="/" element={<Home searchQuery={searchQuery} />} />
+        <Route
+          path="/favorites"
+          element={<Favorites searchQuery={searchQuery} />}
+        />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </Provider>
   );
 }
-
-export default App;
